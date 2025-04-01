2ee24ed68df09cef380c759b121f8aeb07122735
bigcloud07 <dnsxo0712@gmail.com>
2025-04-02 00:58:03 +0900

    ui 마이너 패치2

--- src/components/NewEvent/NewEventList.tsx
+++ src/components/NewEvent/NewEventList.tsx
@@ -89,7 +89,7 @@ const NewEventList: React.FC = () => {
     return (
         <div className="flex flex-col justify-center items-center">
             {/* 상단바 */}
-            <div className="relative flex bg-kuDarkGreen w-[375px] h-[56px] text-white text-xl font-semibold justify-center items-center">
+            <div className="relative flex bg-kuDarkGreen w-[430px] h-[56px] text-white text-xl font-semibold justify-center items-center">
                 <img
                     src={BacbBtnimg}
                     className="absolute left-[24px] cursor-pointer"
@@ -100,7 +100,7 @@ const NewEventList: React.FC = () => {
             </div>
 
             {/* 오늘의 러닝 */}
-            <div className="relative bg-kuDarkGreen w-[375px] min-h-[268px]">
+            <div className="relative bg-kuDarkGreen w-[430px] min-h-[268px]">
                 <div className="w-full flex flex-col items-center pt-2">
                     <div className="w-[114px] h-[32px] bg-white text-kuDarkGreen text-[16px] font-bold rounded-xl flex items-center justify-center">
                         오늘의 행사
